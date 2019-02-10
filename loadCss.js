58197ff8a9f692e71422cb2103dbbd1b8b25806d
shuvaevaei <shuvaeva.ekaterina@gmail.com>
2019-02-10 19:45:10 +0700

    Update loadCss.js

--- loadCss.js
+++ loadCss.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   $.ajax({
-    url: 'https://raw.githubusercontent.com/baevra/slack-dark-theme/master/style.css',
+    url: 'https://raw.githubusercontent.com/shuvaevaei/slack-dark-theme/master/style.css',
     success: function(css) {
       $("<style></style>").appendTo('head').html(css);
     }
